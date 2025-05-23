e2513eb15948e9f1d897844ee887168c33422938
Shinya Sakae <ss49919201@gmail.com>
2025-05-23 22:54:26 +0900

    feat: use hono x-basic

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,16 @@
-import { cloudflare } from '@cloudflare/vite-plugin'
 import build from '@hono/vite-build/cloudflare-workers'
+import adapter from '@hono/vite-dev-server/cloudflare'
+import tailwindcss from '@tailwindcss/vite'
+import honox from 'honox/vite'
 import { defineConfig } from 'vite'
-import ssrHotReload from 'vite-plugin-ssr-hot-reload'
 
-export default defineConfig(({ command, isSsrBuild }) => {
-  if (command === 'serve') {
-    return { plugins: [ssrHotReload(), cloudflare()] }
-  }
-  if (!isSsrBuild) {
-    return {
-      build: {
-        rollupOptions: {
-          input: ['./src/style.css'],
-          output: {
-            assetFileNames: 'assets/[name].[ext]'
-          }
-        }
-      }
-    }
-  }
-  return {
-    plugins: [build({ outputDir: 'dist-server' })]
-  }
+export default defineConfig({
+  plugins: [
+    honox({
+      devServer: { adapter },
+      client: { input: ['./app/style.css'] }
+    }),
+    tailwindcss(),
+    build()
+  ]
 })
